0857fe86222f15634f179d7418838685ae5c7a27
Jared White <jared@jaredwhite.com>
2022-05-09 09:53:54 -0700

    Add readme, update styling for mobile

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const fastify = require('fastify')()
 const path = require('path')
 
 fastify
-  .register(require('fastify-static'), {
+  .register(require('@fastify/static'), {
     root: path.join(__dirname, 'public')
   })
   .listen(4001, '0.0.0.0', (err, address) => {
